c2a7adbc18b81b461c6a7b84021a09c0190ea7e1
Omkar-Jadhav <omkar.jadhav00@gmail.com>
2024-02-13 21:52:06 +0530

    commit

--- main.py
+++ main.py
@@ -12,33 +12,33 @@ if __name__ == '__main__':
     # app.run(port=port)
     print("Webhook is listening ")  # Log listening message
 
-VERIFY_TOKEN = "STRAVA"
-python_script = "strava.py"
+# VERIFY_TOKEN = "STRAVA"
+# python_script = "strava.py"
 
-@app.route('/webhook', methods=['POST'])
-def handle_webhook():
-    # data = request.get_json()
-    latest_activity_id = request.args.get('object_id')
-    print("Webhook event received with activity:", latest_activity_id)
+# @app.route('/webhook', methods=['POST'])
+# def handle_webhook():
+#     # data = request.get_json()
+#     latest_activity_id = request.args.get('object_id')
+#     print("Webhook event received with activity:", latest_activity_id)
 
-    # strava.get_latest_activities()
-    # subprocess.run(["python", python_script], check=True, capture_output=True)
-    # stdout = subprocess.check_output(["python", python_script])  # Capture output for logging
-    # print("Python script output:", stdout.decode())
+#     # strava.get_latest_activities()
+#     # subprocess.run(["python", python_script], check=True, capture_output=True)
+#     # stdout = subprocess.check_output(["python", python_script])  # Capture output for logging
+#     # print("Python script output:", stdout.decode())
 
-    return jsonify({"message": "EVENT_RECEIVED"}), 200
+#     return jsonify({"message": "EVENT_RECEIVED"}), 200
 
-@app.route('/webhook', methods=['GET'])
-def verify_webhook():
-    mode = request.args.get('hub.mode')
-    token = request.args.get('hub.verify_token')
-    challenge = request.args.get('hub.challenge')
+# @app.route('/webhook', methods=['GET'])
+# def verify_webhook():
+#     mode = request.args.get('hub.mode')
+#     token = request.args.get('hub.verify_token')
+#     challenge = request.args.get('hub.challenge')
 
-    if mode and token:
-        if mode == 'subscribe' and token == VERIFY_TOKEN:
-            print("Webhook verified")
-            return jsonify({"hub.challenge": challenge})
-        else:
-            return "Invalid verification token", 403
+#     if mode and token:
+#         if mode == 'subscribe' and token == VERIFY_TOKEN:
+#             print("Webhook verified")
+#             return jsonify({"hub.challenge": challenge})
+#         else:
+#             return "Invalid verification token", 403
         
 
